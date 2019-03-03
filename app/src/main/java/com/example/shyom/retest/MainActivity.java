57c6fe94d4b4bf7f743e8380abd7cfd169d68f61
shyom <shyom1d@gmail.com>
2019-03-04 00:30:46 +0300

    ss

--- app/src/main/java/com/example/shyom/retest/MainActivity.java
+++ app/src/main/java/com/example/shyom/retest/MainActivity.java
@@ -1,21 +1,27 @@
 package com.example.shyom.retest;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.Image;
+import android.os.Build;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.lang.Object;
 
 
@@ -27,11 +33,15 @@ public class MainActivity extends AppCompatActivity {
 public static SQLiteHelper sqLiteHelper;
 ImageView imageView,imageView2;
 TextView textView2;
+Button button;
+static Bitmap bm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         sqLiteHelper= new SQLiteHelper(this,"PIC.db");
         String sql="CREATE TABLE IF NOT EXISTS PIC ( pic MEDIUMBLOB )";
@@ -39,19 +49,24 @@ TextView textView2;
 
         imageView=(ImageView)findViewById(R.id.imageView);
         imageView2=(ImageView)findViewById(R.id.imageView2);
-        textView2=(TextView)findViewById(R.id.textView2);
-        ImageToByte();
-    }
+         ImageToByte();
+
+
+
+
+        }
 
 
     public void btn_add(View view) {
+
+
 
 
         try{sqLiteHelper.insertData(ImageToByte());
             Toast.makeText(getApplicationContext(),"added",Toast.LENGTH_SHORT).show();
 
         } catch (Exception e){e.printStackTrace();}
-        imageView.setImageResource(R.mipmap.ic_launcher);
+
 
 
 
@@ -92,11 +107,14 @@ TextView textView2;
 
 
 
-        Bitmap icon = BitmapFactory.decodeResource(this.getResources(),
+       Bitmap icon = BitmapFactory.decodeResource(this.getResources(),
                 R.drawable.logo);
+
         ByteArrayOutputStream stream= new ByteArrayOutputStream();
         icon.compress(Bitmap.CompressFormat.PNG,100,stream);
         byte[] byteArray=stream.toByteArray();
         return byteArray;
     }
+
+
 }
